e6a02d6b38ad11d2ed523b522eeee2ecdd49a836
Varanasi Sakeeth <sakethvaranasi7@gmail.com>
2026-09-04 10:54:13 +0530

    Time: 0 ms (100%), Space: 20.5 MB (79.23%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,10 +3,16 @@ class Solution:
         if not nums:
             return 0
 
-        count = 1
-        for i in range(1,len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[count] = nums[i]
-                count += 1
-        return count
+        # count = 1
+        # for i in range(1,len(nums)):
+        #     if nums[i] != nums[i-1]:
+        #         nums[count] = nums[i]
+        #         count += 1
+        # return count
+        unique = sorted(list(set(nums)))
+
+        for i in range(len(unique)):
+            nums[i] = unique[i]
+
+        return len(unique)
         
